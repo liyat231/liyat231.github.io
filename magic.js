25aa69e6979921c414a34cfde0a8e7b51f32bbeb
liyat231 <liyatkal025@gmail.com>
2026-06-08 20:37:41 -0400

    Delete magic.js

--- magic.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('btn');
-const panel = document.getElementById('panel');
-
-
-       btn.addEventListener('click', function() {
-           panel.classList.toggle('show');
-       });
