4e8316b29d2bd7f10a9f9932a5c75dd20c3c6ff2
liyat231 <liyatkal025@gmail.com>
2026-06-08 20:39:21 -0400

    Delete form.js

--- form.js
+++
@@ -1,6 +0,0 @@
- document.getElementById('requestForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    alert("Thank you! Your request has been submitted.");
-    this.reset();
-});
-
